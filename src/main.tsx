7c057d04ed884669c1dfb03a81af026640ae5774
Karen Sanasaryan <sanasaryan.k@gmail.com>
2026-01-26 13:22:50 +0400

    Delete src directory

--- src/main.tsx
+++
@@ -1,8 +0,0 @@
-import { createRoot } from 'react-dom/client'
-import 'leaflet/dist/leaflet.css'
-import './index.css'
-import App from './App.tsx'
-
-createRoot(document.getElementById('root')!).render(
-  <App />
-)
